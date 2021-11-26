7b9d021ea8aa286d6bbff8a9130f6d64f8082668
achdiathadit <achdiat.dev@gmail.com>
2021-11-26 10:16:51 +0700

    Create context

--- src/contexts/ResultContextProvider.js
+++ src/contexts/ResultContextProvider.js
@@ -3,4 +3,44 @@ import React, { createContext, useContext, useState } from 'react';
 const ResultContext = createContext();
 const baseUrl = 'https://google-search3.p.rapidapi.com/api/v1';
 
-export const ResultContextProvider = ({ children }) => {};
+export const ResultContextProvider = ({ children }) => {
+	const [results, setResults] = useState([]);
+	const [isLoading, setIsLoading] = useState(false);
+	const [searchTerm, setSearchTerm] = useState('JS Mastery');
+
+	const getResults = async (type) => {
+		setIsLoading(true);
+
+		const response = await fetch(`${baseUrl}${type}`, {
+			method: 'GET',
+			headers: {
+				'x-user-agent': 'desktop',
+				'x-proxy-location': 'US',
+				'x-rapidapi-host': 'google-search3.p.rapidapi.com',
+				'x-rapidapi-key': '355875bdf5msh65ee03d2390bba2p16c176jsn1dd98f538b86',
+			},
+		});
+
+		const data = await response.json();
+
+		if (type.includes('/news')) {
+			setResults(data.entries);
+		} else if (type.includes('/images')) {
+			setResults(data.image_results);
+		} else {
+			setResults(data.results);
+		}
+
+		setIsLoading(false);
+	};
+
+	return (
+		<ResultContext.Provider
+			value={{ getResults, results, searchTerm, setSearchTerm, isLoading }}
+		>
+			{children}
+		</ResultContext.Provider>
+	);
+};
+
+export const useResultContext = () => useContext(ResultContext);
